2d21452cb79abcf5de0ab395915ea7896c87c9e5
Sherbet <sherbetkabylbekova@gmail.com>
2021-06-15 00:13:10 +0600

    Artificcial Intelligence

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+//Show menu
+
+const modalBtn = document.querySelector('.header-sigin')
+const modalWrapper = document.querySelector('.modal-wrapper')
+
+modalBtn.addEventListener('click', () => {
+  modalWrapper.classList.add('show')
+})
